8e56b41586a67814a570d99ec22caeae8fa50939
Alexander Ostrovsky <alex@aleos.dev>
2026-02-06 22:02:13 +0530

    Implement matching logic

--- CodeBreaker/CodeBreaker/CodeBreaker.swift
+++ CodeBreaker/CodeBreaker/CodeBreaker.swift
@@ -17,7 +17,7 @@ struct CodeBreaker {
     
     mutating func attemptGuess() {
         var attempt = guess
-        attempt.kind = .attempt
+        attempt.kind = .attempt(guess.match(against: masterCode))
         attempts.append(attempt)
     }
     
@@ -38,7 +38,37 @@ struct Code {
     
     static let missing: Peg = .clear
     
-    enum Kind {
-        case master, guess, attempt, unknown
+    enum Kind: Equatable {
+        case master
+        case guess
+        case attempt([Match])
+        case unknown
+    }
+    
+    var matches: [Match] {
+        switch kind {
+        case .attempt(let matches): matches
+        default: Array(repeating: Match.nomatch, count: pegs.count) // FIXME: The array is only to reserve space for matches in the UI when the matches weren't displayed
+        }
+    }
+    
+    func match(against otherCode: Code) -> [Match] {
+        var results: [Match] = Array(repeating: .nomatch, count: pegs.count)
+        var pegsToMatch = otherCode.pegs
+        for index in pegs.indices.reversed() {
+            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+                results[index] = .exact
+                pegsToMatch.remove(at: index)
+            }
+        }
+        for index in pegs.indices {
+            if results[index] != .exact {
+                if let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+                    results[index] = .inexact
+                    pegsToMatch.remove(at: matchIndex)
+                }
+            }
+        }
+        return results
     }
 }
