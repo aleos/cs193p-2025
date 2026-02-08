812269b70fe4e04a22af2a4daba7c671d1e27a8f
Alexander Ostrovsky <alex@aleos.dev>
2026-02-08 19:56:10 +0530

    Spruce up the code views

--- CodeBreaker/CodeBreaker/UI/CodeBreakerView.swift
+++ CodeBreaker/CodeBreaker/UI/CodeBreakerView.swift
@@ -18,13 +18,17 @@ struct CodeBreakerView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                view(for: game.masterCode)
+                CodeView(code: game.masterCode)
                 ScrollView {
                     if !game.isOver {
-                        view(for: game.guess)
+                        CodeView(code: game.guess, selection: $selection) { guessButton }
                     }
                     ForEach(game.attempts.indices.reversed(), id: \.self) { index in
-                        view(for: game.attempts[index])
+                        CodeView(code: game.attempts[index]) {
+                            if let matches = game.attempts[index].matches {
+                                MatchMarkers(matches: matches)
+                            }
+                        }
                     }
                 }
                 PegChooser(choices: game.pegChoices) {
@@ -63,22 +67,6 @@ struct CodeBreakerView: View {
     func restart() {
         withAnimation {
             game = CodeBreaker(numberOfPegs: selectedNumberOfPegs)
-        }
-    }
-    
-    func view(for code: Code) -> some View {
-        HStack {
-            CodeView(code: code, selection: $selection)
-            Color.clear.aspectRatio(1, contentMode: .fit)
-                .overlay {
-                    if let matches = code.matches {
-                        MatchMarkers(matches: matches)
-                    } else {
-                        if code.kind == .guess {
-                            guessButton
-                        }
-                    }
-                }
         }
     }
     
