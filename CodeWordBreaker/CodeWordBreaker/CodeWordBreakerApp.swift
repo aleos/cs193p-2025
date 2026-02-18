588a77de56d19e975ae604a040bf773791501f04
Alexander Ostrovsky <alex@aleos.dev>
2026-02-18 23:07:12 +0530

    Create a new project: CodeWordBreaker

---
+++ CodeWordBreaker/CodeWordBreaker/CodeWordBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWordBreakerApp.swift
+//  CodeWordBreaker
+//
+//  Created by Alexander Ostrovsky on 18/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeWordBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
