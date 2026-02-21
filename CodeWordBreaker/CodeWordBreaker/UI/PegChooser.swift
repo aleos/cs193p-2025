a77ef75e0bbf2de9c04c036415ed9d524dcd42d8
Alexander Ostrovsky <alex@aleos.dev>
2026-02-21 23:42:39 +0530

    Arrange peg keyboard

--- CodeWordBreaker/CodeWordBreaker/UI/PegChooser.swift
+++ CodeWordBreaker/CodeWordBreaker/UI/PegChooser.swift
@@ -13,19 +13,59 @@ struct PegChooser: View {
     
     // MARK: Data Out Function
     let onChoose: ((Peg) -> Void)?
+    
+    @State private var measuredWidth: CGFloat = 200
         
     // MARK: - Body
     
     var body: some View {
+        VStack {
+            row(for: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"], width: measuredWidth - Key.outerPadding)
+            row(for: ["a", "s", "d", "f", "g", "h", "j", "k", "l"], width: measuredWidth - Key.outerPadding)
+            row(for: ["z", "x", "c", "v", "b", "n", "m"], width: measuredWidth - Key.outerPadding)
+        }
+        .overlay(
+            GeometryReader { proxy in
+                Color.clear
+                    .preference(key: WidthPreferenceKey.self, value: proxy.size.width)
+            }
+        )
+        .onPreferenceChange(WidthPreferenceKey.self) { newWidth in
+            measuredWidth = newWidth
+        }
+    }
+    
+    func row(for choices: [Peg], width: CGFloat) -> some View {
         HStack {
-            ForEach(choices, id: \.self) { peg in
-                Button {
-                    onChoose?(peg)
-                } label: {
-                    PegView(peg: peg)
+            Spacer()
+            HStack(spacing: Key.spacing) {
+                ForEach(choices, id: \.self) { peg in
+                    Button {
+                        onChoose?(peg)
+                    } label: {
+                        PegView(peg: peg)
+                            .padding(Key.innerPadding)
+                            .background(RoundedRectangle(cornerRadius: 5).strokeBorder(.gray))
+                    }
+                    .frame(width: width / CGFloat(Key.maxNumber) - Key.spacing)
                 }
             }
+            Spacer()
         }
+    }
+}
+
+fileprivate struct Key {
+    static let outerPadding: CGFloat = 0
+    static let innerPadding: CGFloat = 2
+    static let spacing: CGFloat = 10
+    static let maxNumber = 10
+}
+
+private struct WidthPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
 
