c1564939566ffdd29243578749cee5a69054c4d3
Alexander Ostrovsky <alex@aleos.dev>
2026-03-30 19:33:02 +0530

    Remove restart functionality from game view

--- CodeWordBreaker/CodeWordBreaker/UI/CodeWordBreakerView.swift
+++ CodeWordBreaker/CodeWordBreaker/UI/CodeWordBreakerView.swift
@@ -14,8 +14,6 @@ struct CodeWordBreakerView: View {
     // MARK: Data Owned by Me
     @State private var game = CodeWordBreaker()
     @State private var selection = 0
-    @State private var selectedNumberOfPegs = 4
-    @State private var restarting = false
     @State private var invalidGuessCount = 0
     @State private var checker = UITextChecker()
     
@@ -26,11 +24,10 @@ struct CodeWordBreakerView: View {
             VStack {
                 CodeView(code: game.masterCode)
                 ScrollView {
-                    if !game.isOver || restarting {
+                    if !game.isOver {
                         CodeView(code: game.guess, selection: $selection)
                             .transaction { $0.animation = nil }
                             .modifier(ShakeEffect(shakes: invalidGuessCount))
-                            .opacity(restarting ? 0 : 1)
                     }
                     ForEach(game.attempts.indices.reversed(), id: \.self) { index in
                         CodeView(code: game.attempts[index])
@@ -47,26 +44,9 @@ struct CodeWordBreakerView: View {
             .toolbar {
                 if game.masterCode.hasMissingPegs {
                     ProgressView()
-                } else {
-                    Button("Restart", systemImage: "arrow.circlepath") {
-                        restart()
-                    }
                 }
             }
             .navigationTitle(game.selectedTheme.capitalized)
-            .toolbar {
-                ToolbarItem(placement: .bottomBar) {
-                    Picker("Number of pegs", selection: $selectedNumberOfPegs) {
-                        ForEach(3...6, id: \.self) {
-                            Text("^[\($0) letters](inflect: true)")
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                    .onChange(of: selectedNumberOfPegs) {
-                        restart()
-                    }
-                }
-            }
             .navigationBarTitleDisplayMode(.inline)
         }
         // words are loaded asynchronously, so when they arrive,
@@ -99,19 +79,6 @@ struct CodeWordBreakerView: View {
         withAnimation(.guess) {
             game.attemptGuess()
             selection = 0
-        }
-    }
-    
-    func restart() {
-        withAnimation(.restart) {
-            restarting = true
-        } completion: {
-            invalidGuessCount = 0
-            withAnimation(.restart) {
-                game.restart(numberOfPegs: selectedNumberOfPegs, masterWord: words.random(length: selectedNumberOfPegs))
-                selection = 0
-                restarting = false
-            }
         }
     }
 }
