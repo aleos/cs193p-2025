165b522ae434e761f6f2a12d1836a12d7a9354a7
Alexander Ostrovsky <alex@aleos.dev>
2026-04-11 14:10:02 +0400

    Add SwiftDataPreview modifier

---
+++ CodeWordBreaker/CodeWordBreaker/UI/Extensions/SwiftDataPreview.swift
@@ -0,0 +1,28 @@
+//
+//  SwiftDataPreview.swift
+//  CodeWordBreaker
+//
+//  Created by Alexander Ostrovsky on 11/4/2026.
+//
+
+
+import SwiftData
+import SwiftUI
+
+struct SwiftDataPreview: PreviewModifier {
+    static func makeSharedContext() async throws -> ModelContainer {
+        try ModelContainer(for: CodeWordBreaker.self, configurations: .init(isStoredInMemoryOnly: true))
+    }
+    
+    func body(content: Content, context: ModelContainer) -> some View {
+        content.modelContainer(context)
+    }
+}
+
+extension PreviewTrait<Preview.ViewTraits> {
+    @MainActor static var swiftData: Self = .modifier(SwiftDataPreview())
+}
+
+#Preview(traits: .modifier(SwiftDataPreview())) {
+    Text("Hello, World!")
+}
