3622bebc05b92c6da24bc2812aa745e88a80de8c
Alexander Ostrovsky <alex@aleos.dev>
2026-04-11 14:45:34 +0400

    Move game list logic from GameChooser into GameList

--- CodeWordBreaker/CodeWordBreaker/UI/GameList.swift
+++ CodeWordBreaker/CodeWordBreaker/UI/GameList.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct GameList: View {
     // MARK: Data In
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.words) var words
+    @Environment(\.settings) var settings
     
     // MARK: Data Shared with Me
     @Binding var selection: CodeWordBreaker?
@@ -26,24 +28,77 @@ struct GameList: View {
                     GameSummary(game: game)
                         .allowsHitTesting(false)
                 }
-                .swipeActions(edge: .leading) {
-                    NavigationLink(value: game.masterCode.word) {
-                        Label("Cheat", systemImage: "eye")
-                            .tint(.purple)
-                    }
+                .contextMenu {
+                    deleteButton(for: game)
                 }
             }
             .onDelete { offsets in
-                offsets
-                    .map { games[$0] }
-                    .forEach { modelContext.delete($0) }
+                for offset in offsets {
+                    modelContext.delete(games[offset])
+                }
             }
         }
         .listStyle(.plain)
-        .onAppear(perform: addSampleGames)
-        .navigationDestination(for: String.self) { word in
-            Text(word).font(.largeTitle)
+        .onChange(of: words.count) { oldValue, newValue in
+            guard oldValue == 0, newValue > 0 else { return }
+            initializeMasterCodes()
         }
+        .onAppear(perform: addSampleGames)
+        .onAppear {
+            guard words.count > 0 else { return }
+            initializeMasterCodes()
+        }
+        .toolbar {
+            if words.count == 0 {
+                ProgressView()
+            } else {
+                newGame
+            }
+            EditButton()
+        }
+    }
+    
+    private var newGame: some View {
+        Menu("New game", systemImage: "plus") {
+            ForEach(3...6, id: \.self) { numberOfLetters in
+                Button("^[\(numberOfLetters) letters](inflect: true)") {
+                    settings.defaultWordLength = numberOfLetters
+                    createNewGame()
+                }
+            }
+        } primaryAction: {
+            createNewGame()
+        }
+    }
+    
+    private func createNewGame() {
+        let game = CodeWordBreaker()
+        initializeMasterCode(for: game)
+        withAnimation {
+            modelContext.insert(game)
+        }
+        selection = game
+    }
+    
+    private func deleteButton(for game: CodeWordBreaker) -> some View {
+        Button("Delete", systemImage: "minus.circle", role: .destructive) {
+            withAnimation {
+                modelContext.delete(game)
+            }
+        }
+    }
+    
+    private func initializeMasterCodes() {
+        games.forEach(initializeMasterCode)
+    }
+    
+    private func initializeMasterCode(for game: CodeWordBreaker) {
+        guard game.masterCode.hasMissingPegs else { return } // The master code has already been set
+        guard let word = words.random(length: settings.defaultWordLength) else {
+            assertionFailure("Can't set master code: no words")
+            return
+        }
+        game.restart(masterWord: word)
     }
     
     private func addSampleGames() {
